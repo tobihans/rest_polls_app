2e30128a5f1a8c5d40d992e6ff4cc7e8b1726c5d
Hans Tognon <learningway403@gmail.com>
2022-03-29 16:10:15 +0100

    feat: adding test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+from polls import apiviews
+
+class TestPoll(APITestCase):
+    def setUp(self) -> None:
+        self.factory = APIRequestFactory()
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)  # type: ignore
+        self.view = apiviews.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            username='test',
+            email='test@example.org',
+            password='test',
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri, HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         f'Expected Response Code 200 but received {response.status_code}')
