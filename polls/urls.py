596812302c05891942ab0be5d20e9f9279b6222d
Hans Tognon <learningway403@gmail.com>
2022-03-29 11:37:19 +0100

    feat: base API with Django only

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
 
+from .views import polls_detail, polls_list
+
+
+urlpatterns = [
+    path('polls/', polls_list, name='polls_list'),
+    path('polls/<int:pk>/', polls_detail, name='polls_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpRequest, JsonResponse
 
-# Create your views here.
+from .models import Poll
+
+
+def polls_list(request: HttpRequest) -> JsonResponse:
+    print('Called')
+    MAX_OBJECTS = 20
+    polls = Poll.objects.all()[:MAX_OBJECTS]
+    data = {'results': list(polls.values('question', 'created_by__username', 'published_at'))}
+    return JsonResponse(data)
+
+def polls_detail(request: HttpRequest, pk: int) -> JsonResponse:
+    poll = get_object_or_404(Poll, pk=pk)
+    data = {'result': {
+        'question': poll.question,
+        'created_by': poll.created_by.username,
+        'published_at': poll.published_at,
+    }}
+    return JsonResponse(data)
